e5a42acd5edd84ded635cf1bc4778c1fd85c5b51
Dinusha Premakumara <sdbl679@sdb.lk>
2023-04-20 16:34:48 +0530

    My first commit

---
+++ Test.py
@@ -0,0 +1,22 @@
+ print(ast.dump(ast.parse("""
+
+while x:
+
+   ...
+
+else:
+
+   ...
+
+"""), indent=4))
+Module(
+    body=[
+        While(
+            test=Name(id='x', ctx=Load()),
+            body=[
+                Expr(
+                    value=Constant(value=Ellipsis))],
+            orelse=[
+                Expr(
+                    value=Constant(value=Ellipsis))])],
+    type_ignores=[])
